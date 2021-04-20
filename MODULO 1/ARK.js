2bb0691908888932703c85b7b9f0c6466f14ae8c
ivan.go <argivan05@gmail.com>
2021-04-19 21:45:17 -0300

    ejemplo

---
+++ MODULO 1/ARK.js
@@ -0,0 +1,10 @@
+let readlineSync = require('readline-sync');
+let Altura  = readlineSync.questionFloat("Altura Requerida:");
+console.log("Ingrese Altura Requerida:".Altura);
+let Tu = readlineSync.question("Tu Altura:");
+console.log("ingrese Tu Altura:".Tu);
+if (Altura <= Tu){
+    console.log("si puedes pasar.");
+} else {
+    console.log("no puedes pasar.");
+}
